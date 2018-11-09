f90dd55be7b6f838f09844078c13852ea2f678b3
Robert Usey <ruusey@gmail.com>
2018-11-09 16:17:33 -0500

    Fixed some things. Added heap sort

--- CS146/src/com/sw/SWSort.java
+++ CS146/src/com/sw/SWSort.java
@@ -1,23 +1,24 @@
 package com.sw;
 
-import java.awt.List;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Scanner;
 
 public class SWSort {
-    public static final ArrayList<String> queries = new ArrayList<String>();
+    // HashMap to store the counts of each word
+
     public static HashMap<Integer, String> wordCounts = new HashMap<Integer, String>();
-    public final static String[] toFind = new String[] {"force", "Wookiee", "Jabba","lightsaber", "Skywalker", "Tatooine", "Bantha", "Wedge", "Death Star"};
+    // List of words we are looking for
+    public final static String[] toFind = new String[] { "force", "Wookiee", "Jabba", "lightsaber", "Skywalker",
+	    "Tatooine", "Bantha", "Wedge", "Death Star" };
+
     public static void main(String[] args) throws IOException {
-	System.out.println("Scanning Star Wars and sorting the words “force”, “wookiee”, “jabba”, ”lightsaber”, “Skywalker”, “Tatooine”, “Bantha”, “Wedge”, “death star”");
+	System.out.println(
+		"Scanning Star Wars and sorting the words “force”, “wookiee”, “jabba”, ”lightsaber”, “Skywalker”, “Tatooine”, “Bantha”, “Wedge”, “death star”");
 	String allText = "";
 	File in = new File("src//com//sw//sw.txt");
 	BufferedReader br = new BufferedReader(new FileReader(in));
@@ -26,22 +27,23 @@ public class SWSort {
 	while ((st = br.readLine()) != null) {
 	    allText += st;
 	}
-	   
-	
+
 	br.close();
 	countWords(allText);
-	
+
     }
+
+    // Count the number of occurences for each given word
     public static void countWords(String text) {
-	int fCount = countMatches(text,toFind[0]);
-	int wCount = countMatches(text,toFind[1]);
-	int jCount = countMatches(text,toFind[2]);
-	int lCount = countMatches(text,toFind[3]);
-	int sCount = countMatches(text,toFind[4]);
-	int tCount = countMatches(text,toFind[5]);
-	int bCount = countMatches(text,toFind[6]);
-	int weCount = countMatches(text,toFind[7]);
-	int dCount = countMatches(text,toFind[8]);
+	int fCount = countMatches(text, toFind[0]);
+	int wCount = countMatches(text, toFind[1]);
+	int jCount = countMatches(text, toFind[2]);
+	int lCount = countMatches(text, toFind[3]);
+	int sCount = countMatches(text, toFind[4]);
+	int tCount = countMatches(text, toFind[5]);
+	int bCount = countMatches(text, toFind[6]);
+	int weCount = countMatches(text, toFind[7]);
+	int dCount = countMatches(text, toFind[8]);
 	wordCounts.put(fCount, toFind[0]);
 	wordCounts.put(wCount, toFind[1]);
 	wordCounts.put(jCount, toFind[2]);
@@ -51,69 +53,78 @@ public class SWSort {
 	wordCounts.put(bCount, toFind[6]);
 	wordCounts.put(weCount, toFind[7]);
 	wordCounts.put(dCount, toFind[8]);
-	sortTable();
-	
+	// Sort our hashmap based on its integer counts using Collections.sort
+	// 
+	sortTable(true);
+
     }
-    public static void sortTable() {
-	 ArrayList<Integer> v = new ArrayList<Integer>(wordCounts.keySet());
-	 Collections.sort(v);
-	 for (int str : v) {
-	      System.out.println("The number of time the word '"+wordCounts.get(str) + "' appears in Star Wars is " + str );
+
+    // Build a hashmap to keep track of the counts of each word while sorting
+    public static void sortTable(boolean sortWithAntiquatedSortingMethod) {
+	if (sortWithAntiquatedSortingMethod) {
+	    ArrayList<Integer> v = new ArrayList<Integer>(wordCounts.keySet());
+	    Integer[] arr = new Integer[v.size()];
+	    v.toArray(arr);
+	    sort(arr);
+	    for (int i : arr) {
+		System.out.println(
+			"The number of time the word '" + wordCounts.get(i) + "' appears in Star Wars is " + i);
 	    }
+	} else {
+	    ArrayList<Integer> v = new ArrayList<Integer>(wordCounts.keySet());
+	    Collections.sort(v);
+	    for (int str : v) {
+		System.out.println(
+			"The number of time the word '" + wordCounts.get(str) + "' appears in Star Wars is " + str);
+	    }
+	}
     }
+
+    // Count number of matches of word ignoring lower/Upercase within a string
     private static int countMatches(String str, String sub) {
-	str=str.toLowerCase(); sub=sub.toLowerCase();
-	    int count = 0;
-	    if(!str.isEmpty() && !sub.isEmpty()) {
-	        for (int i = 0; (i = str.indexOf(sub, i)) != -1; i += sub.length()) {
-	            count++;
-	        }
+	str = str.toLowerCase();
+	sub = sub.toLowerCase();
+	int count = 0;
+	if (!str.isEmpty() && !sub.isEmpty()) {
+	    for (int i = 0; (i = str.indexOf(sub, i)) != -1; i += sub.length()) {
+		count++;
 	    }
-	    return count;
-    }
-    public void sort(int arrA[]) {
-        int size = arrA.length;
-
-        // Build heap
-        for (int i = size / 2 - 1; i >= 0; i--)
-            heapify(arrA, size, i);
-
-        // One by one extract (Max) an element from heap and
-        // replace it with the last element in the array
-        for (int i=size-1; i>=0; i--) {
-
-            //arrA[0] is a root of the heap and is the max element in heap
-            int x = arrA[0];
-            arrA[0] = arrA[i];
-            arrA[i] = x;
-
-            // call max heapify on the reduced heap
-            heapify(arrA, i, 0);
-        }
+	}
+	return count;
     }
 
-    // To heapify a subtree with node i
-    void heapify(int arrA[], int heapSize, int i) {
-        int largest = i; // Initialize largest as root
-        int leftChildIdx  = 2*i + 1; // left = 2*i + 1
-        int rightChildIdx  = 2*i + 2; // right = 2*i + 2
+    public static void sort(Integer arr[]) {
+	int n = arr.length;
 
-        // If left child is larger than root
-        if (leftChildIdx  < heapSize && arrA[leftChildIdx ] > arrA[largest])
-            largest = leftChildIdx ;
+	for (int i = n / 2 - 1; i >= 0; i--)
+	    heapify(arr, n, i);
 
-        // If right child is larger than largest so far
-        if (rightChildIdx  < heapSize && arrA[rightChildIdx ] > arrA[largest])
-            largest = rightChildIdx ;
+	for (int i = n - 1; i >= 0; i--) {
+	    int temp = arr[0];
+	    arr[0] = arr[i];
+	    arr[i] = temp;
 
-        // If largest is not root
-        if (largest != i) {
-            int swap = arrA[i];
-            arrA[i] = arrA[largest];
-            arrA[largest] = swap;
-
-            // Recursive call to  heapify the sub-tree
-            heapify(arrA, heapSize, largest);
-        }
+	    heapify(arr, i, 0);
+	}
     }
+
+    static void heapify(Integer arr[], int n, int i) {
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+
+	if (l < n && arr[l] > arr[largest])
+	    largest = l;
+
+	if (r < n && arr[r] > arr[largest])
+	    largest = r;
+	if (largest != i) {
+	    int swap = arr[i];
+	    arr[i] = arr[largest];
+	    arr[largest] = swap;
+
+	    heapify(arr, n, largest);
+	}
+    }
+
 }
